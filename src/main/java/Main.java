ee682092036c3daf5f9c0f8d32e0633afe93ac24
root <root@DESKTOP-VDRK620.>
2024-11-05 18:07:42 +0100

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+package movement;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Game game = new Game(scanner);
+        game.start();
+
+    }
+}
